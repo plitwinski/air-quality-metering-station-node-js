61757985a9f281a4f0d5c9e992294f6b89c2d480
Piotr Litwinski <piotr.litwinski@hotmail.com>
2017-02-19 18:02:45 +0000

    Add vsc config files to enable debugging.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,6 @@ startScheduler()
 startServer()
 console.log('Press any key to stop')
 
-process.stdin.setRawMode(true)
-process.stdin.resume()
 process.stdin.on('data', () => {
   console.log('Stopping web server')
   stopServer()
