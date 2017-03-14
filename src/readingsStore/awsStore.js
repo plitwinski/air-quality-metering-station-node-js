931adbeaf6f2b065024b496f591fefd4d277de4c
Piotr Litwinski <piotr.litwinski@hotmail.com>
2017-03-14 20:56:39 +0000

    Add further AWS IoT integration improvements.

--- src/readingsStore/awsStore.js
+++ src/readingsStore/awsStore.js
@@ -1,18 +1,19 @@
 import path from 'path'
 import awsIoT from 'aws-iot-device-sdk'
+import uuid from 'node-uuid'
 import config from '../config.json'
 
 import eventAggregator from '../events/eventAggregator'
 import { PM_READING_STARTED, PM_READING_FINISHED } from '../events/eventConstants'
 
-const appRoot = path.dirname(require.main.filename)
+const appRoot = './'
 const topicName = config.awsIoT.topicName
 
 const device = awsIoT.device({
   keyPath: path.resolve(appRoot, config.awsIoT.keyPath),
   certPath: path.resolve(appRoot, config.awsIoT.certPath),
   caPath: path.resolve(appRoot, config.awsIoT.caPath),
-  clientId: `air-quality-metering-station_${config.awsIoT.clientId}`,
+  clientId:`air-quality-metering-station_${config.awsIoT.clientId}`,
   region: config.awsIoT.region
 })
 
@@ -31,14 +32,13 @@ const readingFinished = (readings) => {
   if (isConnected) {
     readings.forEach((reading) => {
       const data = {
+        id: uuid.v4(),
         deviceType: reading.deviceName,
         'PM2.5': reading.PM25,
         PM10: reading.PM10,
-        localTime: new Date()
-      }
-
-      if (config.awsIoT.location) {
-        data.location = config.awsIoT.location
+        location: config.awsIoT.location,
+        localTime: new Date(),
+        clientId: config.awsIoT.clientId
       }
 
       device.publish(topicName, JSON.stringify(data))
